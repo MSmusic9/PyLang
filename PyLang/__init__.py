55b8e821d0eb7dceeb71290595378c9d7f1fea7d
MSmusic9 <121720254+MSmusic9@users.noreply.github.com>
2023-04-29 19:24:17 +0300

    Update __init__.py

--- PyLang/__init__.py
+++ PyLang/__init__.py
@@ -4,12 +4,9 @@ from termcolor import colored as clr
 
 
 class CompileLang:
-	def __init__(self,langname:str,filepath,langtype,compilepath):
-		self.langtype = langtype
-		self.compilepath = compilepath
-		self.filepath = filepath
+	def __init__(self):
 		self.file = open(self.filepath)
-		self.langname = langname
+		self.compiler = open("main.c","a")
 	def onEveryLine(self,line:str,command):
 		self.line = line
 		for self.line in self.file.readlines():
@@ -19,48 +16,9 @@ class CompileLang:
 	def execOn(self,linecom:str,execline:str):
 		if self.line == linecom:
 			self.compiler.write(execline)
-	def showMessage(self,deco:str,color:str,effect:str,msgtype:str):
-		if msgtype == 'info':
-			print(clr(deco,color,attrs=[effect]))
-		if msgtype == 'error':
-			print(clr(deco,color,attrs=[effect]))
-			exit(0)
-	def run(self,command):
-		self.compiler = open(
-			f"PyLang/run/{self.compilepath}.{self.langtype}",
-			"w"
-		)
-		self.compiler = open(
-			f"PyLang/run/{self.compilepath}.{self.langtype}",
-			"a"
-		)
+	def showMessage(self,deco:str,color:str,effect:str):
+		print(clr(deco,color,attrs=[effect]))
+	def run(self):
 		self.compiler.close()
-		os.system(
-			command.format(
-				lang_name
-				=
-				"PyLang/run/"
-				+
-				self.compilepath
-				+
-				"."
-				+
-				self.langtype
-			)
-		)
-		os.remove(
-			f"PyLang/run/{self.compilepath}.{self.langtype}"
-		)
-	def writeJSONfile(self,version,pattern,path,license_,author,description):
-		f = open(f"{path}{self.filepath}.json","w")
-		f.write("{"+"""
-  "language_name": "{lang}",
-  "version": "{ver}",
-	"description": "{des}",
-  "compiler": "GCC",
-  "lisense": "{lis}",
-  "author": "{auth}",
-  "pattern": "{pat}"
-""".format(des=description,lang=self.langname,ver=version,lis=license_,auth=author,pat=pattern)+"}"
-		)
-		f.close()
+		os.system("gcc main.c -o main")
+		os.remove("main.c")
