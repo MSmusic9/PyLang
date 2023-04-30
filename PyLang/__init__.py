0d9dc5d956558a171f51a78a476859b7d670dbea
MSmusic9 <121720254+MSmusic9@users.noreply.github.com>
2023-04-30 14:08:03 +0300

    Update __init__.py

--- PyLang/__init__.py
+++ PyLang/__init__.py
@@ -4,7 +4,7 @@ from termcolor import colored as clr
 
 class CompilerLang:
 	def __init__(self,filepath):
-                self.filepath = filepath
+    self.filepath = filepath
 		self.file = open(self.filepath)
 		self.compiler = open("main.c","a")
 	def onEveryLine(self,line:str,command):
@@ -16,25 +16,25 @@ class CompilerLang:
 	def execOn(self,linecom:str,execline:str):
 		if self.line == linecom:
 			self.compiler.write(execline)
-        def setMainStruct(self):
-                self.compiler.write('#include "stdio.h"\n')
+  def setMainStruct(self):
+    self.compiler.write('#include "stdio.h"\n')
 	def showMessage(self,deco:str,color:str,effect:str):
 		print(clr(deco,color,attrs=[effect]))
 	def run(self):
 		self.compiler.close()
-                self.file.close()
+    self.file.close()
 		os.system("gcc main.c -o main")
 		os.remove("main.c")
 
 
 class InterplerLang(CompilerLang):
-        def __new__(self,filepath):
-                self.filepath = filepath
-                self.file = open(self.filepath)
-        def run(self):
-                self.file.close()
-        def setMainStruct(self):
-                pass
-        def execOn(self,linecom,execline):
-                if self.line == linecom:
-                        exec(execline)
+  def __new__(self,filepath):
+    self.filepath = filepath
+    self.file = open(self.filepath)
+  def run(self):
+    self.file.close()
+  def setMainStruct(self):
+    pass
+  def execOn(self,linecom,execline):
+    if self.line == linecom:
+      exec(execline)
