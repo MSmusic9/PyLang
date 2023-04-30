1feb2891a18565628a534b7e97ddfcf7e0597cac
MSmusic9 <121720254+MSmusic9@users.noreply.github.com>
2023-04-30 14:05:16 +0300

    Update __init__.py

--- PyLang/__init__.py
+++ PyLang/__init__.py
@@ -1,11 +1,10 @@
 import os
-os.system("pip install numba && pip install termcolor")
-from numba import njit
 from termcolor import colored as clr
 
 
 class CompilerLang:
 	def __init__(self,filepath):
+                self.filepath = filepath
 		self.file = open(self.filepath)
 		self.compiler = open("main.c","a")
 	def onEveryLine(self,line:str,command):
@@ -17,6 +16,8 @@ class CompilerLang:
 	def execOn(self,linecom:str,execline:str):
 		if self.line == linecom:
 			self.compiler.write(execline)
+        def setMainStruct(self):
+                self.compiler.write('#include "stdio.h"\n')
 	def showMessage(self,deco:str,color:str,effect:str):
 		print(clr(deco,color,attrs=[effect]))
 	def run(self):
@@ -28,9 +29,12 @@ class CompilerLang:
 
 class InterplerLang(CompilerLang):
         def __new__(self,filepath):
+                self.filepath = filepath
                 self.file = open(self.filepath)
         def run(self):
                 self.file.close()
+        def setMainStruct(self):
+                pass
         def execOn(self,linecom,execline):
                 if self.line == linecom:
                         exec(execline)
